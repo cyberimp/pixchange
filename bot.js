caea4bbee016ccc7cd0d3386bd087f0c88bd1ba1
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-24 03:40:07 +0400

    try download

--- bot.js
+++ bot.js
@@ -31,6 +31,11 @@ router.post('/' + token, function(req,res){
             console.error('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
+            request("https://api.telegram.org/bot"+ token +"/"+body.result.file_path,function (error,response,body){
+                console.error('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+        });
         });
     res.status(200).send('ok'); 
 });
