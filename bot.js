20b511a401f28b309576e45d1a5bd71cddbc1583
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-24 01:58:24 +0400

    meh

--- bot.js
+++ bot.js
@@ -5,9 +5,16 @@ var request = require ('request');
 var router = express.Router();
 
 const token = process.env.TOKEN;
+const hookURI = process.env.HOOK;
 
-router.get('/', function(req, res){
-    res.status(200).send('bot setup here');
+router.get('/setup', function(req, res){
+    request("https://api.telegram.org/bot"+ token +
+    "/setWebhook?url="+encodeURIComponent(hookURI + token),function (error,response,body){
+        console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+    res.status(200).send('bot setup done');
 })
 
 router.post('/' + token, function(req,res){
