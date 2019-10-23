28ef5f703d7b09f93f25c9df9d0606324d468c64
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-24 02:11:57 +0400

    logging of messages

--- bot.js
+++ bot.js
@@ -11,14 +11,16 @@ router.get('/setup', function(req, res){
     request("https://api.telegram.org/bot"+ token +
     "/setWebhook?url="+encodeURIComponent(hookURI + token),function (error,response,body){
         console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.status(200).send(body);
     });
     res.status(200).send('bot setup done');
 })
 
 router.post('/' + token, function(req,res){
-
+    console.log(req.body);
+    res.status(200).send('ok'); 
 });
 
 module.exports = router;
