4e2a369682442bbb2fac1c9402684ba407a2db01
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-24 22:08:56 +0400

    test proxy

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict';
 var express = require('express');
 var request = require ('request');
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-west-2'});
+var S3 = new AWS.S3();
+
 var bot = require('./bot');
+
+var { Client } = require('pg');
 
 const app = express();
 const port = process.env.PORT;
 const token = process.env.TOKEN;
+const bucket = process.env.S3_BUCKET;
 const chatID = 123;
 
 app.use('/bot', bot)
@@ -14,17 +21,20 @@ app.get('/favicon.ico', (req, res) => {
   res.send('');
 });
 
-app.post('/:app', (req, res) => {
-  const trackingData =
-    '*App name:* `' + req.params.app + '`\n' +
-    '*IP:* `' + req.headers['x-forwarded-for'] +'`\n' +
-    '*User-Agent:* `' + req.headers['user-agent'] + '`\n' +
-    '*Referer:* `' + req.headers['referer'] + '`';
-    request("https://api.telegram.org/bot"+ token + "/sendDocument").form({})
-    res.send('');
-});
-
-app.get('/:app', (req, res) => {
+app.get('/:img', (req, res) => {
+  S3.getObject({Bucket: bucket, Key:req.params.img}).on("httpHeaders",(code,headers,resp, message) =>{
+    res.status.code(code).headers(headers)})
+    .on("httpData",(chunk) => {res.send(chunk)})
+    .on("complete",(resp) =>{
+      console.log("Done!");
+    })
+  var client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  client.connect().then(function(){
+    S3  
+  })
   const trackingData =
     '*App name:* `' + req.params.app + '`\n' +
     '*IP:* `' + req.headers['x-forwarded-for'] +'`\n' +
