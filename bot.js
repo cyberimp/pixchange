aa22df508e6a631affe5f353987a063260872f05
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-24 20:09:32 +0400

    meh

--- bot.js
+++ bot.js
@@ -52,8 +52,8 @@ router.post('/' + token, function(req,res){
                     var imagename = uuid() +'.'+ ext;
                     S3.upload({Body: resp, Bucket: bucket, Key: imagename},function(err, data) {
                         client.connect();
-                        client.query('INSERT INTO images(image_id, message_id, chat_id, push,comment) VALUES (\''+
-                        imagename+'\','+message_id+','+chatID+',true,\''+ comment+'\');',function(err,res){
+                        client.query('INSERT INTO images(image_id, message_id, chat_id, push, comment) VALUES (\''+
+                        imagename+'\','+message_id+','+chatID+',true, \''+ comment+'\');',function(err,res){
                             console.log(err);
                             console.log(res);
                             client.end();
