b6f4dce87f0a5df16e85c0c253bba9e40056e7c2
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-26 20:25:27 +0400

    final bot commands version

--- bot.js
+++ bot.js
@@ -16,16 +16,16 @@ const bucket = process.env.S3_BUCKET;
 AWS.config.update({region:'us-west-2'});
 var S3 = new AWS.S3();
 
-router.get('/setup', function(req, res){
-    request("https://api.telegram.org/bot"+ token +
-    "/setWebhook?url="+encodeURIComponent(hookURI + token),function (error,response,body){
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        res.status(200).send(body);
-    });
-    res.status(200).send('bot setup done');
-})
+// router.get('/setup', function(req, res){
+//     request("https://api.telegram.org/bot"+ token +
+//     "/setWebhook?url="+encodeURIComponent(hookURI + token),function (error,response,body){
+//         console.error('error:', error); // Print the error if one occurred
+//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//         console.log('body:', body); // Print the HTML for the Google homepage.
+//         res.status(200).send(body);
+//     });
+//     res.status(200).send('bot setup done');
+// })
 
 router.post('/' + token, function(req,res){
         if (!('message' in req.body)){
@@ -45,7 +45,7 @@ router.post('/' + token, function(req,res){
                 connectionString: process.env.DATABASE_URL,
                 ssl: true,
               });
-            var query = 'SELECT * FROM images WHERE chat_id='+chatID+
+            var query = 'SELECT image_id FROM images WHERE chat_id='+chatID+
             ' AND message_id='+req.body.edited_message.message_id+';';
             console.log(query);
             client.connect((err)=>{
@@ -65,15 +65,15 @@ router.post('/' + token, function(req,res){
                             request("https://api.telegram.org/file/bot"+ token +"/"+result.result.file_path).on("response",function(resp){
                                 if(200 == resp.statusCode){
                                     S3.upload({Body: resp, Bucket: bucket, Key: imagename},function(err, data) {
-                                                console.log(err);
-                                                var message = 'Uploaded new version.';
-                                                request("https://api.telegram.org/bot"+ token +
-                                                "/sendMessage?chat_id=" + chatID +
-                                                "&parse_mode=Markdown" +
-                                                "&reply_to_message_id="+ message_id+
-                                                "&text="+ encodeURIComponent(message)).on("complete",function(resp){
-                                                    client.end();
-                                                });
+                                        console.log(err);
+                                        var message = 'Uploaded new version.';
+                                        request("https://api.telegram.org/bot"+ token +
+                                        "/sendMessage?chat_id=" + chatID +
+                                        "&parse_mode=Markdown" +
+                                        "&reply_to_message_id="+ message_id+
+                                        "&text="+ encodeURIComponent(message)).on("complete",function(resp){
+                                            client.end();
+                                        });
                                     });
                                 }
                             });
