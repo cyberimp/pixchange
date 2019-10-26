94f3485f7e3ca2e55ede1dec8ddb8493e9d9369b
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-26 19:32:09 +0400

    new functions

--- bot.js
+++ bot.js
@@ -29,11 +29,86 @@ router.get('/setup', function(req, res){
 
 router.post('/' + token, function(req,res){
         if (!('message' in req.body)){
+            if(!('edited_message' in req.body)){
+                res.sendStatus(200);
+                return;
+            }
+            var chatID = req.body.edited_message.chat.id;
+            if (!('photo' in req.body.edited_message)){
+                request("https://api.telegram.org/bot"+ token +
+                "/sendMessage?chat_id=" + chatID +
+                "&text="+encodeURIComponent("Please attach a photo to update image on link."));
+                res.sendStatus(200);
+                return;
+            }
+            var query = 'SELECT image_id FROM images WHERE chat_id='+chatID+
+            ' AND message_id='+req.body.edited_message.message_id+';';
+            console.log(query);
             res.sendStatus(200);
+            client.query(query,function(err,res){
+                if(err == null && res.rows.length>0){
+                    var largest = req.body.edited_message.photo.slice(-1).pop();
+                    var imagename = res.rows[0].image_id;
+                    request("https://api.telegram.org/bot"+ token +
+                    "/getFile?file_id="+largest.file_id,function (error,response,body){
+                        console.error('error:', error); // Print the error if one occurred
+                        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                        console.log('body:', body); // Print the HTML for the Google homepage.
+                        var result = JSON.parse(body);
+                        console.log(result.result);
+                        request("https://api.telegram.org/file/bot"+ token +"/"+result.result.file_path).on("response",function(resp){
+                            if(200 == resp.statusCode){
+                                S3.upload({Body: resp, Bucket: bucket, Key: imagename},function(err, data) {
+                                            var message = 'Uploaded new version.';
+                                            request("https://api.telegram.org/bot"+ token +
+                                            "/sendMessage?chat_id=" + chatID +
+                                            "&parse_mode=Markdown" +
+                                            "&reply_to_message_id="+ message_id+
+                                            "&text="+ encodeURIComponent(message)).on("complete",function(resp){
+                                                client.end();
+                                            });
+                                });
+                            }
+                        });
+                    });
+                }
+            });
             return;
+
         } 
         var chatID = req.body.message.chat.id;
         if (!('photo' in req.body.message)){
+            if (!('text') in req.body.message){
+                res.sendStatus(200);
+                return;
+            }
+            var text = req.body.message.text;
+            if (text == '/start'){
+                var client = new Client({
+                    connectionString: process.env.DATABASE_URL,
+                    ssl: true,
+                  });
+                client.connect(function(err){
+                    console.log(err);
+                    var query = 'SELECT image_id FROM images WHERE chat_id='+chatID+';';
+                    client.query(query,(err, res) =>{
+                        res.rows.forEach(element => {
+                            var image = element.image_id;
+                            S3.deleteObject({Bucket: bucket, Key: image});        
+                        });
+                        query = 'DELETE FROM images WHERE chat_id='+chatID+';';
+                        client.query(query,(err, res) =>{
+                            client.end();
+                        });
+                    });
+                    res.sendStatus(200);
+
+                    request("https://api.telegram.org/bot"+ token +
+                    "/sendMessage?chat_id=" + chatID +
+                    "&text="+encodeURIComponent("Your records are now clear, send images for tracking."));
+                });
+                return;
+            }
             var command = req.body.message.text.toLowerCase().replace(/[^a-zа-я]/g,'');
             if (['хватит', 'stop', 'заебал', 'выруби', 'off', 'вырубай'].indexOf(command)!=-1){
                 if (!('reply_to_message' in req.body.message)){
@@ -50,6 +125,9 @@ router.post('/' + token, function(req,res){
                             client.end();
                         });
                     });
+                    request("https://api.telegram.org/bot"+ token +
+                    "/sendMessage?chat_id=" + chatID +
+                    "&text="+encodeURIComponent("Turned off push messages globally."));
                     return;
                 }
                 var client = new Client({
@@ -66,6 +144,9 @@ router.post('/' + token, function(req,res){
                         client.end();
                     });
                 });
+                request("https://api.telegram.org/bot"+ token +
+                "/sendMessage?chat_id=" + chatID +
+                "&text="+encodeURIComponent("Turned off push messages for image."));
                 return;
             }
 
@@ -84,6 +165,9 @@ router.post('/' + token, function(req,res){
                             client.end();
                         });
                     });
+                    request("https://api.telegram.org/bot"+ token +
+                    "/sendMessage?chat_id=" + chatID +
+                    "&text="+encodeURIComponent("Turned on push messages globally."));
                     return;
                 }
                 var client = new Client({
@@ -100,6 +184,9 @@ router.post('/' + token, function(req,res){
                         client.end();
                     });
                 });
+                request("https://api.telegram.org/bot"+ token +
+                "/sendMessage?chat_id=" + chatID +
+                "&text="+encodeURIComponent("Turned on push messages for image."));
                 return;
             }
 
@@ -111,7 +198,6 @@ router.post('/' + token, function(req,res){
         }
         var largest = req.body.message.photo.slice(-1).pop();
         var message_id = req.body.message.message_id;
-        var comment = req.body.message.caption;
         console.log(chatID);
         console.log(req.body);
         console.log(largest);
